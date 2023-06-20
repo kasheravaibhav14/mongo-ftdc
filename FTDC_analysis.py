c3ac85a55b5379d873341e50cfa4946ddece898b
Vaibhav Kashera <kashera@vaibhav.kashera-C02DR5QRMD6N>
2023-06-20 12:05:26 +0530

    idea 1: OR across deltas

--- FTDC_analysis.py
+++ FTDC_analysis.py
@@ -10,13 +10,16 @@ from sklearn.ensemble import IsolationForest
 from matplotlib import pyplot as plt
 from pprint import pprint
 
+
 class FTDC_an:
     def __init__(self, metricObj, qTstamp):
         self.metricObj = metricObj
         self.queryTimeStamp = qTstamp
         self.ticketlim = 15  # when available tickets are more than 15, we assume that the system is in normal state or is heading toward crash
+        self.tdelta = 10
+        self.threshold = 0.25
 
-    def __plot(self, df, to_monitor, vert_x): 
+    def __plot(self, df, to_monitor, vert_x):
         to_monitor.sort()
         print("Monitoring: ", str(to_monitor))
         print(len(to_monitor))
@@ -48,13 +51,13 @@ class FTDC_an:
         rename_cols = {}
         for col_name in df.columns:
             ccol = col_name.replace("serverStatus.", "ss ")
-            ccol = ccol.replace("wiredTiger.", "WT ")
-            ccol = ccol.replace("systemMetrics", "sm ")
+            ccol = ccol.replace("wiredTiger.", "wt ")
+            ccol = ccol.replace("systemMetrics.", "sm ")
             ccol = ccol.replace("tcmalloc.tcmalloc", "tcmalloc")
-            ccol = ccol.replace("transaction.transaction", "txn ")
+            ccol = ccol.replace("transaction.transaction", "txn")
             ccol = ccol.replace("transaction.", "txn ")
-            ccol = ccol.replace("local.oplog.rs.stats", "locOplogRsStats ")
-            ccol = ccol.replace("aggStageCounters", "aggCnt ")
+            ccol = ccol.replace("local.oplog.rs.stats.", "locOplogRsStats ")
+            ccol = ccol.replace("aggStageCounters.", "aggCnt ")
             print(col_name, ccol)
             rename_cols[col_name] = ccol
         df.rename(columns=rename_cols, inplace=True)
@@ -62,7 +65,6 @@ class FTDC_an:
     def __meanCalc(self, df, tbounds):
         curr_mean = {}
         prev_mean = {}
-
         for col in df.columns:
             curr_mean[col] = df.iloc[tbounds['c_lb']
                 :tbounds['c_ub']+1][col].mean()
@@ -139,10 +141,10 @@ class FTDC_an:
 
     def __tcmallocminuswt(self, metricObj):
         wtcache = "serverStatus.wiredTiger.cache.bytes currently in the cache"
-                #    "serverStatus.wiredTiger.cache.bytes allocated for updates",
-                #    "serverStatus.wiredTiger.cache.bytes dirty in the cache cumulative"]
+        #    "serverStatus.wiredTiger.cache.bytes allocated for updates",
+        #    "serverStatus.wiredTiger.cache.bytes dirty in the cache cumulative"]
         tcmalloc = "serverStatus.tcmalloc.generic.current_allocated_bytes"
-                    # "serverStatus.tcmalloc.tcmalloc.current_total_thread_cache_bytes"]
+        # "serverStatus.tcmalloc.tcmalloc.current_total_thread_cache_bytes"]
         newkey = "serverStatus.wiredTiger.tcmalloc derived: allocated minus wt cache MiB"
         if wtcache not in metricObj or tcmalloc not in metricObj:
             return
@@ -153,22 +155,43 @@ class FTDC_an:
         for idx in range(len(metricObj[wtcache])):
             metricObj[newkey].append(
                 (metricObj[tcmalloc][idx]-metricObj[wtcache][idx])/(mib_conv))
-    
-    def checkMetric(self,curr_mean,prev_mean,met):
-        if prev_mean[met] != 0 and (abs(curr_mean[met]-prev_mean[met])/prev_mean[met]) > 0.25:
-            return True
-        if met == "ss WT cache.bytes dirty in the cache cumulative" or met.startswith("ss WT concurrentTransactions"):
-            return True
 
+    def checkMetric(self, curr_mean, prev_mean, met):
+        if prev_mean[met] != 0 and (abs(curr_mean[met]-prev_mean[met])/prev_mean[met]) > self.threshold:
+            return True
+        if met == "ss wt cache.bytes dirty in the cache cumulative" or (met.startswith("ss wt concurrentTransactions") and not met.endswith("totalTickets")):
+            return True
+    def calcBounds(self,df,pos,delt):
+        tbounds = {'t0': -1, 'c_ub': -1, 'c_lb': -
+                   1, 'p_lb': -1}  # p_ub is equal to c_lb
+        tdelta = timedelta(seconds=delt)
+        for idx in range(pos, -1, -1):
+            if df.iloc[idx]['ss wt concurrentTransactions.write.available'] > self.ticketlim \
+                    and df.iloc[idx]['ss wt concurrentTransactions.read.available'] > self.ticketlim:
+                tbounds['t0'] = idx
+                break
+        t0_val = df.index[tbounds['t0']]
+        for idx in range(tbounds['t0'], len(df), 1):
+            if df.index[idx]-t0_val >= tdelta:
+                tbounds['c_ub'] = idx
+                break
+        for idx in range(tbounds["t0"], -1, -1):
+            if t0_val-df.index[idx] >= tdelta and tbounds['c_lb'] == -1:
+                tbounds['c_lb'] = idx
+            if t0_val-df.index[idx] >= 3*tdelta and tbounds['p_lb'] == -1:
+                tbounds['p_lb'] = idx
+                break
+        return tbounds
+    
     def analytics(self, metricObj, queryTimestamp):
         self.__getAverageLatencies(metricObj)
         self.__tcmallocminuswt(metricObj)
         self.__getMemoryFragRatio(metricObj)
-        un_len = {} #debug
+        un_len = {}  # debug
         for key in metricObj:
             if len(metricObj[key]) not in un_len:
                 # print(key, len(metricObj[key]))
-                un_len[len(metricObj[key])]=[key]
+                un_len[len(metricObj[key])] = [key]
             else:
                 un_len[(len(metricObj[key]))].append(key)
         pprint(un_len)
@@ -190,55 +213,30 @@ class FTDC_an:
         # print(type(queryTimestamp))
         pos = np.where(df.index == queryTimestamp)[0][0]
         # print(pos)
-        tbounds = {'t0': -1, 'c_ub': -1, 'c_lb': -
-                   1, 'p_lb': -1}  # p_ub is equal to c_lb
-        tdelta = timedelta(seconds=30)  # 2.5 minutes delta
-        for idx in range(pos, -1, -1):
-            if df.iloc[idx]['serverStatus.wiredTiger.concurrentTransactions.write.available'] > self.ticketlim \
-                    and df.iloc[idx]['serverStatus.wiredTiger.concurrentTransactions.read.available'] > self.ticketlim:
-                print(
-                    df.iloc[idx]['serverStatus.wiredTiger.concurrentTransactions.write.available'])
-                tbounds['t0'] = idx
-                break
-        t0_val = df.index[tbounds['t0']]
-        for idx in range(tbounds['t0'], len(df), 1):
-            if df.index[idx]-t0_val >= tdelta:
-                tbounds['c_ub'] = idx
-                break
-        for idx in range(tbounds["t0"], -1, -1):
-            if t0_val-df.index[idx] >= tdelta and tbounds['c_lb'] == -1:
-                tbounds['c_lb'] = idx
-            if t0_val-df.index[idx] >= 3*tdelta and tbounds['p_lb'] == -1:
-                tbounds['p_lb'] = idx
-                break
-        print(tbounds)
-        for k in tbounds:
-            print(k, df.index[tbounds[k]])
         # self.__findOutliersWithZscore(
         #     df, 'serverStatus.wiredTiger.concurrentTransactions.write.out')
         # self.__findOutliersWithZscore(df, 'serverStatus.wiredTiger.concurrentTransactions.read.out')
         self.__renameCols(df)
-        # return
-        curr_mean, prev_mean = self.__meanCalc(df, tbounds)
         to_monitor = []
-        for metric in curr_mean:
-            try:
-                if self.checkMetric(curr_mean,prev_mean,metric):
-                    # if True:
-                    to_monitor.append(metric)
-                    print(
-                        metric, 100*(curr_mean[metric]-prev_mean[metric])/prev_mean[metric])
-            except Exception as e:
-                print(e, "unable to insert metric")
-        # print(df[to_monitor])
-        # print(df.iloc[tbounds['p_lb']:tbounds['c_lb']+1][to_monitor])
-        # print('------')
-        # print(df.iloc[tbounds['c_lb']:tbounds['c_ub']+1][to_monitor])
+        # return
+        tbounds=self.calcBounds(df,pos,self.tdelta)
+        for delt in range(self.tdelta,10*self.tdelta+1,self.tdelta):
+            curr_mean, prev_mean = self.__meanCalc(df, tbounds)
+            for metric in curr_mean:
+                try:
+                    if self.checkMetric(curr_mean, prev_mean, metric):
+                        # if True:
+                        if metric not in to_monitor:
+                            to_monitor.append(metric)
+                            print(
+                                metric, 100*(curr_mean[metric]-prev_mean[metric])/prev_mean[metric])
+                except Exception as e:
+                    print(e, "unable to insert metric")
+            tbounds = self.calcBounds(df,pos,delt)
         self.__plot(df[tbounds['p_lb']:tbounds['c_ub']+1],
                     to_monitor, vert_x=queryTimestamp)
-        # self.__generatePDF(df[tbounds['p_lb']:tbounds['c_ub']+1],to_monitor,vert_x=queryTimestamp)
 
-    def parseAll(self, queryTimeStamp=''):
+    def parseAll(self):
         def delta(metrList):
             mylst = [metrList[i] for i in range(len(metrList))]
             for i in range(1, len(metrList)):
@@ -251,7 +249,7 @@ class FTDC_an:
             elif met.startswith("systemMetrics.disks") and (met.endswith("reads") or met.endswith("writes") or met.endswith("read_time_ms") or met.endswith("write_time_ms")):
                 return True
             return False
-        
+
         data = {}
         iter_keys = iter(self.metricObj)
         # extract the first level of data
@@ -271,38 +269,43 @@ class FTDC_an:
         deltactr = len(metObj["serverStatus.start"])
         delta1 = 0
         for met in metObj:
-            if met in sel_metr_p :
+            if met in sel_metr_p:
                 data[met] = metObj[met]
 
         for met in metObj:
-            if met in sel_metr_c or checkCriteria(met): #checkCriteria implements string matching for certain cumulative metrics
+            # checkCriteria implements string matching for certain cumulative metrics
+            if met in sel_metr_c or checkCriteria(met):
                 data[met] = delta(metObj[met])
 
         for key in iter_keys:
             metObj = self.metricObj[key]
-            sel_metr_c_new = [s for s in metObj.keys() if (s in sel_metr_c or checkCriteria(s))]
+            sel_metr_c_new = [s for s in metObj.keys() if (
+                s in sel_metr_c or checkCriteria(s))]
             sel_metr_p_new = [s for s in metObj.keys() if s in sel_metr_p]
 
             '''
             handle edge case that a certain metric gets acquired halfway which was not present in the initial list
             eg. serverStatus.locks.Global.acquireWaitTime
             '''
-            new_c = [item for item in sel_metr_c_new if item not in data] # metric not in data ever before
+            new_c = [
+                item for item in sel_metr_c_new if item not in data]  # metric not in data ever before
             new_p = [item for item in sel_metr_p_new if item not in data]
 
             for met in new_c:
-                data[met] = [0 for i in range(deltactr)] # add zeros for those metric who have never been there in the data before
+                # add zeros for those metric who have never been there in the data before
+                data[met] = [0 for i in range(deltactr)]
                 print("occurence of new accumulate metric", met)
 
             for met in new_p:
-                data[met] = [0 for i in range(deltactr)] 
+                data[met] = [0 for i in range(deltactr)]
                 print("occurence of new point metric", met)
 
             for met in sel_metr_p_new:
-                data[met].extend(metObj[met]) # now fill all the values obtained
+                # now fill all the values obtained
+                data[met].extend(metObj[met])
             for met in sel_metr_c_new:
                 data[met].extend(delta(metObj[met]))
-            delta1=len(metObj[sel_metr_p_new[0]])
+            delta1 = len(metObj[sel_metr_p_new[0]])
 
             # handle the case where unusual number of nmetrics or ndeltas occur,
             # i.e. less metrics are reported compared to previous iteration, so fill with zeros for point in time data and previous value for accumulative data
@@ -312,7 +315,6 @@ class FTDC_an:
                 elif met not in sel_metr_c_new and met not in sel_metr_p:
                     prev_val = data[met][-1]
                     data[met].extend([prev_val] * delta1)
-
 
             deltactr += len(metObj["serverStatus.start"])
         print(len(data.keys()))
